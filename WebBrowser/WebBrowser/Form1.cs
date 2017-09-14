15e36ebfe399fee11f372dd2b546f13fc876685f
STUDENTS\1795845 <mhira.ali.hana@gmail.com>
2017-09-14 11:23:06 -0400

    * new application windows forms c#

---
+++ WebBrowser/WebBrowser/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebBrowser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox1.Text)) // testing if textbox is empty 
+                MessageBox.Show(string.Format("Hello {0}", textBox1.Text));
+            else
+                MessageBox.Show("Hello No Name User");
+
+
+            textBox1.Text = "";
+        }
+    }
+}
