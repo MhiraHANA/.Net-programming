2946aed4fb9b5ba5342e1dab39555d12a638cc67
STUDENTS\1795845 <mhira.ali.hana@gmail.com>
2017-09-18 13:33:27 -0400

    *test net programming

---
+++ GeometryConsoleApplication/GeometryConsoleApplication/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryConsoleApplication
+{
+    public abstract class Shape
+    {
+       
+        public abstract double GetArea();
+    }
+
+    public class Rectangle : Shape
+    {
+        public double Height { get; set; }
+        public double Width { get; set; }
+        public override double GetArea()
+        {
+            return Height * Width;
+        }
+    }
+
+    public class Circle : Shape
+    {
+        public double Radius { get; set; }
+        public override double GetArea() { return Math.PI * Radius * Radius; }
+    }
+
+    public class Triangle : Shape
+    {
+        public double Height { get; set; }
+        public double Width { get; set; }
+        public override double GetArea()
+        {
+            return Width * Height / 2;
+        }
+        
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("There is no args");
+                Console.ReadKey();
+            }
+
+            if (args.Length == 3)
+            {
+                if (!string.IsNullOrEmpty(args[0]))
+                {
+                    Console.WriteLine("Display the area of ");
+                    Console.WriteLine(args[0]);
+                    Rectangle r = new Rectangle();
+                    r.Width = 10.2;
+                    r.Height = 20.3;
+                    Console.WriteLine(r.GetArea());
+                    Console.ReadKey();
+                }
+
+                if (!string.IsNullOrEmpty(args[1]))
+                {
+                    Console.WriteLine("\n Display the area of ");
+                    Console.WriteLine(args[1]);
+                    Circle c = new Circle();
+                    c.Radius = 5.6;
+                    Console.WriteLine(c.GetArea());
+                    Console.ReadKey();
+                }
+                if (!string.IsNullOrEmpty(args[2]))
+                {
+                    Console.WriteLine("\n Display the area of ");
+                    Console.WriteLine(args[2]);
+                    Triangle t = new Triangle();
+                    t.Height = 4.3;
+                    t.Width = 5.9;
+                    Console.WriteLine(t.GetArea());
+                    Console.ReadKey();
+                }
+               
+            }
+        }
+    }
+}
---
+++ WpfAppEmployee/WpfAppEmployee/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppEmployee
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    public partial class MainWindow : Window
+    {
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ViewInfoButton_Click(object sender, RoutedEventArgs e)
+        {
+            switch (ListEmployee.SelectedIndex)
+            {
+                case 0:
+                    
+                  
+                    infoLabel.Content = "Jenifer Emplloyee ID: 0001";
+                    break;
+                case 1:
+                    
+                    infoLabel.Content = "Ali Emplloyee ID: 0002";
+                    break;
+                case 2:
+                    
+                    infoLabel.Content = "Alex Emplloyee ID: 0003";
+                    break;
+                case 3:
+                    
+                    infoLabel.Content = "Jack Emplloyee ID: 0004";
+                    break;
+                case 4:
+                    
+                    infoLabel.Content = "Hana Emplloyee ID: 0005";
+                    break;
+                default:
+                    
+                    infoLabel.Content = "";
+                    break;
+
+
+            }
+        }
+
+    }
+}
+
